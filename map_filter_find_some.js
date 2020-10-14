889be1a352dfe9a2c4c9d5193e81223818fbe70d
WesleyNogueiraJambu <72626040+WesleyNogueiraJambu@users.noreply.github.com>
2020-10-14 13:50:23 -0300

    Mudança do nome do arquivo e adição de novos trechos
    Mudança do nome do arquivo, adição de comentários explicativos sobre os métodos utilizados e adição de novos trechos de retorno de códigos.

--- map_filter_find_some.js
+++ map_filter_find_some.js
@@ -20,7 +20,7 @@ var animais = [{
 
 console.log(animais);
 
-// Map e alterando o valor de nome_tipo
+// Uso do map para mapear os valores de nome_tipo e fazer a alteração do seu valor.
   var map_resultado = animais.map(animal => {
     return animal.nome_tipo = animal.nome + ' - ' + animal.tipo;
   });
@@ -29,25 +29,33 @@ console.log(animais);
 
 // Filter de animais carnívoros
   var filter_resultado = animais.filter(animal => {
-    return animal.tipo.toLowerCase().includes('carnívoro');
+    return animal.tipo.toLowerCase().includes('carnívoro'); // Uso do método includes() para verificar se dentro da string existe o dado 'carnívoro'.
   });
 
   console.log(filter_resultado);
 
-// Find do segundo animal
+// Find do animal com o id 2.
   var find_resultado = animais.find(animal => animal.id === 2);
 
   console.log(find_resultado);
 
-// Some para verificar se existe um padrão
+// Some para verificar se existe um padrão.
   var some_resultado = animais.some(animal => animal.nome === 'Jacaré')
+  true
+// Retorna true se o padrão existir.
 
-// Filter e map
+  var some_resultado = animais.some(animal => animal.nome === 'Macaco')
+  false
+// Retorna false se o padrão não existir.
+
+// Filter para retornar um parâmetro específico e map para mapear os dados e fazer a mudança do campo de primeiro_tipo.
   var parametro = 'mamífero';
   var filter_map_result = animais.filter(animal => {
     return animal.tipo.toLowerCase().includes(parametro);
   }).map(animal => {
     return animal.primeiro_tipo = parametro.charAt(0).toUpperCase() + parametro.slice(1)
+    // Uso dos métodos charAt(0).toUpperCase() para deixar a primeira letra em maiúsculo.
+    // Uso do slice(1) para retornar a string sem a primeira letra.
   });
 
   console.log(filter_map_result);
